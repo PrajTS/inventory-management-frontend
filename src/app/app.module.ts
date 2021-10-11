c2dd27926b29b4fc8eac80930cbbf350bc4a11e7
Prajwal T S <tsprajwal@gmail.com>
2021-10-11 12:12:32 +0530

    Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { InventoryItemComponent } from './inventory/inventory-item/inventory-item.component';
+import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InventoryItemComponent,
+    InventoryListComponent
   ],
   imports: [
     BrowserModule,
